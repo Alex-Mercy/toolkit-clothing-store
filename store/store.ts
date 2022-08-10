5a21c377ad3a362a3578c476f79be2a4152d1aa7
Alex <hambiru@yandex.ru>
2022-08-10 10:20:23 +0300

    hook useActions

---
+++ store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { cartReducer } from './cart/cart.slice'
+import { productApi } from './product/product.api'
+
+export const store = configureStore({
+  reducer: { [productApi.reducerPath]: productApi.reducer, cart: cartReducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware),
+})
+
+export type RootState = ReturnType<typeof store.getState>
